24731a39e2d94f9118719b3134b558eec22ad607
ZP <1034935762@qq.com>
2021-01-30 16:02:06 +0800

    2021年1月30日16:02:06

--- BV1nT4y1P7bj水平滑动背景/script.js
+++ BV1nT4y1P7bj水平滑动背景/script.js
@@ -1,19 +1,17 @@
-const signinBtn = document.getElementById('signin')
-const signupBtn = document.getElementById('signup')
-const firstForm = document.getElementById('from1')
-const secondForm = document.getElementById('from2')
-const container = document.querySelector('.container')
+// 获取元素和点击操作
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
 
-signinBtn.addEventListener('click', () => {
-  container.classList.remove('right-panel-active')
-})
+signInBtn.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
 
-signupBtn.addEventListener('click', () => {
-  container.classList.add('right-panel-active')
-})
-firstForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-})
-secondForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-})
+signUpBtn.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
